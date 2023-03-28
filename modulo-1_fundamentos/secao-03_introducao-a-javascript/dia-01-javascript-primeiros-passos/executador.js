89c30fbc2edc4317d3a180ec9088eef70d7eb3e7
Pitter Pereira <pitterpereira@gmail.com>
2023-03-28 18:39:55 -0300

    Exercícios de hoje

---
+++ modulo-1_fundamentos/secao-03_introducao-a-javascript/dia-01-javascript-primeiros-passos/executador.js
@@ -0,0 +1,19 @@
+let peca = "cavalo";
+
+switch (peca) {
+  case "peao":
+    console.log("Anda para frente 1 quadrado");
+    break;
+  case "cavalo":
+    console.log("Anda 4 quadrados, em formato de L");
+    break;
+  case "bispo":
+    console.log("Anda sem limite nas 2 diagonais");
+    break;
+  case "torre":
+    console.log("Anda sem limite nos eixos vertical e horizontal");
+    break;
+  case "rei":
+    console.log("Anda sem limite nos eixos vertical e horizontal");
+    break;
+}
